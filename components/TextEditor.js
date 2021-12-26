a2eafe5bc8b01ac897f38262085aecf4ad452a14
Burhanudin Syed <burhan@Burhanudins-MacBook-Pro.local>
2021-12-26 16:34:54 -0500

    base complete 2

---
+++ components/TextEditor.js
@@ -0,0 +1,62 @@
+import dynamic from "next/dynamic";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState, convertFromRaw, convertToRaw } from "draft-js";
+import { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { useRouter } from "next/dist/client/router";
+import { useSession } from "next-auth/client";
+import { useDocumentOnce } from "react-firebase-hooks/firestore";
+
+const Editor = dynamic(() =>
+  import("react-draft-wysiwyg").then((module) => module.Editor, { ssr: false })
+);
+
+const TextEditor = () => {
+  const [session] = useSession();
+  const router = useRouter();
+  const {id} = router.query;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const [snapshot] = useDocumentOnce(
+    db.collection("userDocs").doc(session.user.email).collection("docs").doc(id)
+  );
+  useEffect(() => {
+    if (snapshot?.data()?.editorState) {
+      setEditorState(
+        EditorState.createWithContent(
+          convertFromRaw(snapshot?.data()?.editorState)
+        )
+      );
+    }
+  }, [snapshot]);
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+
+    db.collection("userDocs")
+      .doc(session.user.email)
+      .collection("docs")
+      .doc(id)
+      .set(
+        {
+          editorState: convertToRaw(editorState.getCurrentContent()),
+        },
+        {
+          merge: true,
+        }
+      );
+  };
+
+  return (
+    <div className="bg-[#F8F9FA] min-h-screen pb-16">
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={onEditorStateChange}
+        toolbarClassName="flex stick top-0 z-50 !justify-center mx-auto"
+        editorClassName="mt-6 bg-white shadow-lg max-w-4xl mx-auto mb-12 border p-10"
+      />
+    </div>
+  );
+};
+
+export default TextEditor;
